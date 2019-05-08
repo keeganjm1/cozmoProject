da88ef596d9d8b6e823d2456742cca6bf13fc077
alexwcourtney <46701167+alexwcourtney@users.noreply.github.com>
2019-05-08 01:55:50 -0500

    Update iowaCozmo8.py

--- iowaCozmo8.py
+++ iowaCozmo8.py
@@ -87,11 +87,11 @@ def cozmo_program(robot: cozmo.robot.Robot):
 
                     # forward
                     robot.drive_straight(cozmo.util.distance_mm(590.4),#was650.4
-                                         cozmo.util.speed_mmps(110)).wait_for_completed()
-                    robot.turn_in_place(cozmo.util.degrees(92)).wait_for_completed()
+                                         cozmo.util.speed_mmps(100)).wait_for_completed()
+                    robot.turn_in_place(cozmo.util.degrees(90)).wait_for_completed()
                     time.sleep(2)
                     robot.drive_straight(cozmo.util.distance_mm(300.8),# was 320.8
-                                         cozmo.util.speed_mmps(110)).wait_for_completed()
+                                         cozmo.util.speed_mmps(100)).wait_for_completed()
                     # sleep
                     #time.sleep(10)
 
@@ -106,10 +106,10 @@ def cozmo_program(robot: cozmo.robot.Robot):
 
                     # reverse
                     robot.drive_straight(cozmo.util.distance_mm(-300.8),#was 320.8
-                                         cozmo.util.speed_mmps(110)).wait_for_completed()
-                    robot.turn_in_place(cozmo.util.degrees(-92)).wait_for_completed()
+                                         cozmo.util.speed_mmps(100)).wait_for_completed()
+                    robot.turn_in_place(cozmo.util.degrees(-90)).wait_for_completed()
                     robot.drive_straight(cozmo.util.distance_mm(-590.4),
-                                         cozmo.util.speed_mmps(110)).wait_for_completed()
+                                         cozmo.util.speed_mmps(100)).wait_for_completed()
 
                 if item == "O":
                     image = Image.open("o.png")
@@ -231,7 +231,30 @@ def cozmo_program(robot: cozmo.robot.Robot):
                 
                     robot.set_lift_height(1.0).wait_for_completed()
                     robot.turn_in_place(cozmo.util.degrees(-360)).wait_for_completed()
-                    robot.set_lift_height(0).wait_for_completed()                    
+                    robot.set_lift_height(0).wait_for_completed()
+
+                if item == "B":
+                    robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
+                    robot.set_lift_height(0).wait_for_completed()
+
+                    robot.set_lift_height(1).wait_for_completed()
+                    robot.say_text("What a save!", use_cozmo_voice=False, play_excited_animation=True).wait_for_completed()
+                    robot.set_lift_height(0).wait_for_completed()
+
+                    image = Image.open("FSU.png")
+                    image = image.resize(cozmo.oled_face.dimensions(), Image.NEAREST)
+                    image = cozmo.oled_face.convert_image_to_screen_data(image)
+
+                    seconds = 5
+
+                    robot.set_head_angle(cozmo.util.degrees(44.5)).wait_for_completed()
+
+                    for nothing in range(seconds):
+                        robot.display_oled_face_image(image, 1000.0)
+                        time.sleep(1.0)
+                    time.sleep(1.0)
+
+                    robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
 
 
 cozmo.run_program(cozmo_program)
